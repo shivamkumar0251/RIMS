464d6dc7adb2564f3602a2ebc1faf55f18dc4c90
VishalGladhand <developer3.gladhand@gmail.com>
2026-01-16 15:42:48 +0530

    fix acregory vander and brand add form

--- src/components/adminComponents/CreateBrandModal.tsx
+++ src/components/adminComponents/CreateBrandModal.tsx
@@ -68,7 +68,7 @@ const CreateBrandModal: React.FC<CreateBrandModalProps> = ({ open, onClose, onSa
                 </Box>
 
                 <DialogContent className="px-6 pb-6 pt-2">
-                    <Typography variant="body2" className="text-slate-500 mb-8 font-medium">
+                    <Typography variant="body2" className="text-slate-500 pb-4 font-medium">
                         Enter the company or brand name associated with products.
                     </Typography>
 
